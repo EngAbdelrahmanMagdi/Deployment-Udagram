4eef7c6d7d43f5cf07494168d1ad85a1bd2bb943
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-08-04 02:41:37 +0200

    deploying is working fine

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-5pfna4kw.us-east-1.elasticbeanstalk.com/'
+  apiHost: 'http://udagram-api-dev.eba-5pfna4kw.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
